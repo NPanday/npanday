c84735c76f6a79c6613e62f3693ee09a4308a811
Lars Corneliussen <lc@talendos.com>
2018-12-12 12:51:59 +0100

    #2 Fix detection of Framework 4.7.2

--- plugins/netplugins/NPanday.Plugin.Settings/src/main/csharp/SettingsGeneratorMojo.cs
+++ plugins/netplugins/NPanday.Plugin.Settings/src/main/csharp/SettingsGeneratorMojo.cs
@@ -322,22 +322,25 @@ namespace NPanday.Plugin.Settings
                     vendor.frameworks = vendorFrameworks;
                     vendors.Add(vendor);
                 }
-                else if (sdk == "v4.7.2")
-                {
-                    npandaySettingsVendorsVendor vendor = new npandaySettingsVendorsVendor();
-                    vendor.vendorName = "MICROSOFT";
-                    vendor.vendorVersion = "4.7.2";
-                    npandaySettingsVendorsVendorFrameworksFramework[] vendorFrameworks = new npandaySettingsVendorsVendorFrameworksFramework[1];
-                    npandaySettingsVendorsVendorFrameworksFramework vf = new npandaySettingsVendorsVendorFrameworksFramework();
-                    vf.installRoot = dirInfo40.FullName;
-                    vf.frameworkVersion = "4.7.2";
-                    vendorFrameworks[0] = vf;
-                    vf.sdkInstallRoot = sdkInstallRoot40;
-                    FindAndAssignExecutablePaths(vf);
-                    vendor.frameworks = vendorFrameworks;
-                    vendors.Add(vendor);
+                else{
+                    Console.WriteLine("Does not support " + sdk);
                 }
             }
+            if (Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\.NETFramework\v4.0.30319\SKUs\.NETFramework,Version=v4.7.2") != null){
+                npandaySettingsVendorsVendor vendor = new npandaySettingsVendorsVendor();
+                vendor.vendorName = "MICROSOFT";
+                vendor.vendorVersion = "4.7.2";
+                npandaySettingsVendorsVendorFrameworksFramework[] vendorFrameworks = new npandaySettingsVendorsVendorFrameworksFramework[1];
+                npandaySettingsVendorsVendorFrameworksFramework vf = new npandaySettingsVendorsVendorFrameworksFramework();
+                vf.installRoot = dirInfo40.FullName;
+                vf.frameworkVersion = "4.7.2";
+                vendorFrameworks[0] = vf;
+                vf.sdkInstallRoot = sdkInstallRoot40;
+                FindAndAssignExecutablePaths(vf);
+                vendor.frameworks = vendorFrameworks;
+                vendors.Add(vendor);
+            }
+            
             return vendors.ToArray();
         }
 
